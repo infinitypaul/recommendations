e62f1982b6c71b98a14dd059ff434e6d39607552
infinitypaul <infinitypaul@live.com>
2020-06-22 17:44:55 +0100

    getting faker to work on heroku

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-06-22 12:14:32.
+    * Generated on 2020-06-22 16:44:32.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.16.1 on 2020-06-22 12:14:31.
+ * Generated for Laravel 7.16.1 on 2020-06-22 16:44:31.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
